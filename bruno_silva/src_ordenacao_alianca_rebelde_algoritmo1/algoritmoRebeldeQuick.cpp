569042cde13d9887a22142d83922d3157e7c014b
brunosife <brunosife@gmail.com>
2020-10-14 12:43:00 -0300

    Correção no método quick nas comparações dos valores i e j

--- bruno_silva/src_ordenacao_alianca_rebelde_algoritmo1/algoritmoRebeldeQuick.cpp
+++ bruno_silva/src_ordenacao_alianca_rebelde_algoritmo1/algoritmoRebeldeQuick.cpp
@@ -13,8 +13,8 @@ void partirVetor(int esquerda, int direita,
     planetaAuxiliar1 = vetorPlanetas[(*i + *j)/2];
     do{
 
-        while ((planetaAuxiliar1.getDistanciaPlaneta() > vetorPlanetas[*i].getDistanciaPlaneta()) || (planetaAuxiliar1.getDistanciaPlaneta() == vetorPlanetas[*i].getDistanciaPlaneta() && planetaAuxiliar1.getPopulacaoPlaneta() > vetorPlanetas[*i].getPopulacaoPlaneta())) (*i)++;
-        while ((planetaAuxiliar1.getDistanciaPlaneta() < vetorPlanetas[*j].getDistanciaPlaneta()) || (planetaAuxiliar1.getDistanciaPlaneta() == vetorPlanetas[*j].getDistanciaPlaneta() && planetaAuxiliar1.getDistanciaPlaneta() < vetorPlanetas[*j].getPopulacaoPlaneta())) (*j)--;
+        while ((planetaAuxiliar1.getDistanciaPlaneta() > vetorPlanetas[*i].getDistanciaPlaneta()) || (planetaAuxiliar1.getDistanciaPlaneta() == vetorPlanetas[*i].getDistanciaPlaneta() && planetaAuxiliar1.getPopulacaoPlaneta() < vetorPlanetas[*i].getPopulacaoPlaneta())) (*i)++;
+        while ((planetaAuxiliar1.getDistanciaPlaneta() < vetorPlanetas[*j].getDistanciaPlaneta()) || (planetaAuxiliar1.getDistanciaPlaneta() == vetorPlanetas[*j].getDistanciaPlaneta() && planetaAuxiliar1.getPopulacaoPlaneta() > vetorPlanetas[*j].getPopulacaoPlaneta())) (*j)--;
         
         if (*i <= *j){
             planetaAuxiliar2 = vetorPlanetas[*i];
